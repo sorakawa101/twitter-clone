939158202a3150ec51619ad9e709bdd2f874d686
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-06-05 02:08:22 +0900

    add: WidgetsをTwitter関連のライブラリのところ以外実装
    これからライブラリをインストールする

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Timeline from "./components/timeline/Timeline";
-// import Widgets from "./components/widget/Widgets";
+import Widgets from "./components/widget/Widgets";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Timeline />
 
       {/* Widget */}
-      {/* <Widgets /> */}
+      <Widgets />
     </div>
   );
 }
---
+++ src/components/widget/Widgets.jsx
@@ -0,0 +1,38 @@
+import { Search } from "@mui/icons-material";
+import React from "react";
+// import {
+//     TwitterTimelineEmbed,
+//     TwitterShareButton,
+//     TwitterTweetEmbed,
+// } from "react-twitter-embed";
+import "./Widgets.css";
+
+const Widgets = () => {
+    return (
+        <div className="widgets">
+            <div className="widgets_input">
+                <Search className="widgets_searchIcon" />
+                <input placeholder="キーワード検索" type="text" />
+            </div>
+
+            <div className="widgets_widgetContainer">
+                <h2>いまどうしてる？</h2>
+
+                {/* ライブラリを追加・追記 */}
+                {/* <TwitterTweetEmbed tweetId={"1475764828208009220"} />
+
+                <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="Shin_Engineer"
+                options={{ height: 400 }}
+                />
+                <TwitterShareButton
+                url={"https://twitter.com/Shin_Engineer"}
+                options={{ text: "#React.js勉強中", via: "Shin_Engineer" }}
+                /> */}
+            </div>
+        </div>
+    )
+}
+
+export default Widgets
