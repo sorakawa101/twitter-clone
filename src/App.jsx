10e388348dd1e7075a085017f4bf7ed6fd7f59f1
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-06-05 01:04:00 +0900

    add: Sidebarを実装＆MaterialUIを使うためのパッケージをインストール
    具体的には以下の操作
    yarn add @mui/icons-material
    yarn add @mui/material
    yarn add @emotion/react @emotion/styled

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
+// import Timeline from "./components/timeline/Timeline";
+// import Widgets from "./components/widget/Widgets";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Timeline */}
+      {/* <Timeline /> */}
+
+      {/* Widget */}
+      {/* <Widgets /> */}
+    </div>
+  );
+}
+
+export default App;
