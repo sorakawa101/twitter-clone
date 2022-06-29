1a895dda511937ad68c4e53d072162aa5da0969f
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-06-30 00:36:21 +0900

    change: Widgets部分を変更

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { collection, getDocs, query, orderBy, onSnapshot } from "firebase/firestore";
+// import { collection, getDocs, query, orderBy, onSnapshot } from "firebase/firestore";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 import FlipMove from 'react-flip-move';
 
 import "./Timeline.css";
